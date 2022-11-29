48ca8279681b94e47e53ba34770e8192148a6d48
Luis Steinert <76253309+luistnrt@users.noreply.github.com>
2022-11-29 16:28:46 +0100

    update wordEmbedding.ipynb

---
+++ embedUI.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Add a title and intro text
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
