dfd3f61793e4563a3e23c797ea346b5c2acdb19b
Luis Steinert <76253309+luistnrt@users.noreply.github.com>
2022-11-29 16:51:41 +0100

    Update embedUI.py
    Inserted text_input

--- embedUI.py
+++ embedUI.py
@@ -3,5 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Add a title and intro text
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
+st.title('WebEmbedding European UnionConsolidated Financial Sanctions List')
+st.text('This is a web app to fuzzy search possible business partners')
+
+
+title = st.text_input('Business Partner', 'Search')
+st.write('Business Partner is', title)
