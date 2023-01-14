20ba08f628d3f325a75847a25fd115530b37009a
Luis Steinert <76253309+luistnrt@users.noreply.github.com>
2023-01-14 23:27:50 +0100

    created EuropeanSanctions.csv for streamlit

--- Pages/page_2.py
+++ Pages/page_2.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# Page 2")
 st.sidebar.markdown("# Page 2")
+
+df = pd.read_csv("data/EuropeanSanctions.csv")
+
+st.dataframe(df)
--- main_page.py
+++ main_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from thefuzz import fuzz, process 
 
 # Add a title and intro text
 st.title('European UnionConsolidated Financial Sanctions List')
@@ -15,48 +16,11 @@ st.write('Entered name', userInputName)
 #userInputAddress = col2.st.text_input('Enter possible address of partner', 'Search')
 #col2.st.write('Entered address')
 
-# Further imports of functions
-from nltk.tokenize import word_tokenize
-from gensim.models.phrases import Phrases, Phraser
-#import fuzzysearch
-#import fuzzywuzzy
-import PyPDF2
+df = pd.read_csv("data/EuropeanSanctions.csv")
 
-pdfFileObj = open('20221118-FULL.pdf', 'rb') #Create pdf object
-pdfReader = PyPDF2.PdfReader(pdfFileObj) 
-
-allText = ""
-for i in range(792): #Iterating over n pages 
-    pageObj = pdfReader.pages[i]
-    page = pageObj.extract_text()
-    allText += page #Store page content into string variable
 
 # Preprocessing
-allText = allText.replace(';','')
-
-alltext_string_all = allText.replace('Legal basis',';Legal basis').replace('Identity information',';Identity information').replace('Programme', ';Programme').replace('Birth information',';Birth information').replace('Citizenship information',';Citizenship information').replace('Contact information',';Contact information')
-testListSplit = alltext_string_all.split('reference number: EU')
-
-# Create dataframe
-dbn_df = pd.DataFrame([sub.split(";") for sub in testListSplit])
-dbn_df.fillna("",inplace=True)
-dbn_df.columns = ['EU Reference', 'Legal basis', 'Programme', 'Identity information', 'Birth information', 'Citizenship information', 'Contact information']
-dataframe = dbn_df
-
-dataframe['Legal basis'] = dataframe['Legal basis'].apply(lambda x: x.replace('Legal basis: ','').replace('\n',''))
-dataframe['Programme'] = dataframe['Programme'].apply(lambda x: x.replace('Programme: ','').replace('\n',''))
-dataframe['Identity information'] = dataframe['Identity information'].apply(lambda x: x.replace('Identity information:','').replace('Name/Alias:',';').replace('•','').replace('Function:','').split(';'))
-dataframe.replace('^\s+', '', regex=True, inplace=True) #front
-dataframe.replace('\s+$', '', regex=True, inplace=True) #end
-dataframe['Birth information'] = dataframe['Birth information'].apply(lambda x: x.replace('Birth information:\n•',''))
-
-dataframe['Identity information'] = dataframe['Identity information'].apply(lambda x: [y.strip() for y in x ])
-
-
-from thefuzz import fuzz, process 
-
-# Preprocessing
-n = dbn_df['Identity information'].to_string().split()
+n = df['Identity information'].to_string().split()
 
 def findBusinessPartner(input, name, threshold):
     namelist = name.split(" ")
