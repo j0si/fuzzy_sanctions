70d14d12ba2fd4415efe8c240098a27e4a96ffb4
llaptop <j.brenzinger@outlook.de>
2023-01-23 14:02:38 +0100

    df searchable updated

--- main_page.py
+++ main_page.py
@@ -200,108 +200,105 @@ def lcs(S,T):
         st.write(ele)
 
 
-tab1, tab2 = st.tabs(["DEMO","Data"])
 
-with tab1:
-    with col1:
-        st.header('Fuzzy Similarity')
-        findBusinessPartner(userInputName, n)
-    with col2:
-        st.header("Levenshtein Distance")
-        #'Levenshtein Distance', levenshteinDistanceDP(userInputName, n)
-    with col3:
-        st.header('Jaro Winkler Similarity')
-        jaro_winkler(userInputName, n)
-    with col4:
-        st.header('Levenstein Distance')
-        levenshteinDistanceDP(userInputName, n)
-    with col5:
-        st.header('Longest Common Substring')
-        lcs(userInputName, n)
+with col1:
+    st.header('Fuzzy Similarity')
+    findBusinessPartner(userInputName, n)
+with col2:
+    st.header("Levenshtein Distance")
+    #'Levenshtein Distance', levenshteinDistanceDP(userInputName, n)
+with col3:
+    st.header('Jaro Winkler Similarity')
+    jaro_winkler(userInputName, n)
+with col4:
+    st.header('Levenstein Distance')
+    levenshteinDistanceDP(userInputName, n)
+with col5:
+    st.header('Longest Common Substring')
+    lcs(userInputName, n)
         
-with tab2:
-    st.title("Auto Filter Dataframes in Streamlit")
+st.title("Auto Filter Dataframes in Streamlit")
 
-    st.write(
-        """This app accomodates the blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/)
-        and walks you through one example of how the Streamlit
-        Data Science Team builds add-on functions to Streamlit.
-        """
-    )
+st.write(
+    """This app accomodates the blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/)
+    and walks you through one example of how the Streamlit
+    Data Science Team builds add-on functions to Streamlit.
+    """
+)
 
 
-    def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Adds a UI on top of a dataframe to let viewers filter columns
-        Args:
-            df (pd.DataFrame): Original dataframe
-        Returns:
-            pd.DataFrame: Filtered dataframe
-        """
-        modify = st.checkbox("Add filters")
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+    Args:
+        df (pd.DataFrame): Original dataframe
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
 
-        if not modify:
-            return df
-
-        df = df.copy()
-
-        # Try to convert datetimes into a standard format (datetime, no timezone)
-        for col in df.columns:
-            if is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-
-            if is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-
-        modification_container = st.container()
-
-        with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-            for column in to_filter_columns:
-                left, right = st.columns((1, 20))
-                left.write("↳")
-                # Treat columns with < 10 unique values as categorical
-                if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                    user_cat_input = right.multiselect(
-                        f"Values for {column}",
-                        df[column].unique(),
-                        default=list(df[column].unique()),
-                    )
-                    df = df[df[column].isin(user_cat_input)]
-                elif is_numeric_dtype(df[column]):
-                    _min = float(df[column].min())
-                    _max = float(df[column].max())
-                    step = (_max - _min) / 100
-                    user_num_input = right.slider(
-                        f"Values for {column}",
-                        _min,
-                        _max,
-                        (_min, _max),
-                        step=step,
-                    )
-                    df = df[df[column].between(*user_num_input)]
-                elif is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Values for {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
-                else:
-                    user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
-                    )
-                    if user_text_input:
-                        df = df[df[column].str.contains(user_text_input)]
-
+    if not modify:
         return df
 
-    st.dataframe(filter_dataframe(df))
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            left.write("↳")
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    _min,
+                    _max,
+                    (_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].str.contains(user_text_input)]
+
+    return df
+
+st.dataframe(filter_dataframe(df))
