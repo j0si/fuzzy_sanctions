891f9db9017ab407d75e5ff111c4e39112558094
llaptop <j.brenzinger@outlook.de>
2023-01-23 10:12:49 +0100

    levenstein + longest commong substring for demo

--- main_page.py
+++ main_page.py
@@ -20,7 +20,7 @@ st.write('Entered name', userInputName)
 
 df = pd.read_csv("data/EuropeanSanctions.csv")
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5 = st.columns(5)
 
 
 # Preprocessing
@@ -96,6 +96,103 @@ def findBusinessPartner(input, name):
     for element in resultlist:
         st.write(element)
   
+# levenstein implementation
+import numpy as np
+
+def printDistances(distances, token1Length, token2Length):
+    for t1 in range(token1Length + 1):
+        for t2 in range(token2Length + 1):
+            print(int(distances[t1][t2]), end=" ")
+        print()
+
+def levenshteinDistanceDP(token1, token2):
+    
+    # safe results
+    resultlist = []
+    
+    def calculations_levenstein(token1, token2):
+        distances = np.zeros((len(token1) + 1, len(token2) + 1))
+
+        for t1 in range(len(token1) + 1):
+            distances[t1][0] = t1
+
+        for t2 in range(len(token2) + 1):
+            distances[0][t2] = t2
+            
+        a = 0
+        b = 0
+        c = 0
+        
+        for t1 in range(1, len(token1) + 1):
+            for t2 in range(1, len(token2) + 1):
+                if (token1[t1-1] == token2[t2-1]):
+                    distances[t1][t2] = distances[t1 - 1][t2 - 1]
+                else:
+                    a = distances[t1][t2 - 1]
+                    b = distances[t1 - 1][t2]
+                    c = distances[t1 - 1][t2 - 1]
+                    
+                    if (a <= b and a <= c):
+                        distances[t1][t2] = a + 1
+                    elif (b <= a and b <= c):
+                        distances[t1][t2] = b + 1
+                    else:
+                        distances[t1][t2] = c + 1
+
+        # print matrix
+        # printDistances(distances, len(token1), len(token2))
+        return distances[len(token1)][len(token2)]
+
+    for element in token2:
+        resultlist.append([calculations_levenstein(token1, element), element])
+    resultlist.sort()
+    resultlist = resultlist[:5]
+    print(resultlist)
+    
+    st.write("Top 5 results:")
+    for ele in resultlist:
+        st.write(ele)
+
+
+# longest common substring
+
+def lcs(S,T):
+    
+    resultlist = []
+    
+    def lcs_calc(S,T):
+        m = len(S)
+        n = len(T)
+        counter = [[0]*(n+1) for x in range(m+1)]
+        longest = 0
+        lcs_set = set()
+        for i in range(m):
+            for j in range(n):
+                if S[i] == T[j]:
+                    c = counter[i][j] + 1
+                    counter[i+1][j+1] = c
+                    if c > longest:
+                        lcs_set = set()
+                        longest = c
+                        lcs_set.add(S[i-c+1:i+1])
+                    elif c == longest:
+                        lcs_set.add(S[i-c+1:i+1])
+
+        return lcs_set
+
+    for element in T:
+        resultlist.append([lcs_calc(S, element), element])
+    resultlist.sort(reverse=True)
+    resultlist = resultlist[:5]
+    
+    print(resultlist)
+    
+    st.write("Top 5 results:")
+    for ele in resultlist:
+        st.write(ele)
+
+
+
 
 
 
@@ -108,3 +205,9 @@ with col2:
 with col3:
     st.header('Jaro Winkler Similarity')
     jaro_winkler(userInputName, n)
+with col4:
+    st.header('Levenstein Distance')
+    levenshteinDistanceDP(userInputName, n)
+with col5:
+    st.header('Longest Common Substring')
+    lcs(userInputName, n)
