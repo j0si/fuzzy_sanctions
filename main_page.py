7877f371aeb50aa61a3daadf63bd0e5de59a04db
llaptop <j.brenzinger@outlook.de>
2023-02-10 08:50:57 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -321,7 +321,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     f"Substring or regex in {column}",
                 )
                 if user_text_input:
-                    df = df[df[column].str.contains(user_text_input)]
+                    df = df[df[column].str.contains(user_text_input, na=False)]
 
     return df
 
